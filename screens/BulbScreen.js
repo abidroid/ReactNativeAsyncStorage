c32b6e9c84084be26d6c7f9dcd38b57e634c04ee
Muhammad Abid <abidroid1@gmail.com>
2025-05-17 11:06:28 +0500

    screens

---
+++ screens/BulbScreen.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+
+const BulbScreen = () => {
+
+    return (
+        <View><Text>Bulb Screen</Text></View>
+    );
+};
+
+export default BulbScreen;
---
+++ screens/CounterScreen.js
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+
+const CounterScreen = () => {
+ return (
+        <View><Text>Counter Screen</Text></View>
+    );
+};
+
+export default CounterScreen;
