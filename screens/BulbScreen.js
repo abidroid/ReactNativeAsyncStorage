79409158ac3d7b2a5a38f5c83f61dba814045fb0
Muhammad Abid <abidroid1@gmail.com>
2025-05-17 15:17:52 +0500

    switch component

--- screens/BulbScreen.js
+++ screens/BulbScreen.js
@@ -1,10 +1,37 @@
-import {View, Text} from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, Switch } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
 
 const BulbScreen = () => {
 
+    const [bulbOn, setBulbChange] = React.useState(false)
+
+    const toggleSwitch = () => setBulbChange(previousState => !previousState);
+
     return (
-        <View><Text>Bulb Screen</Text></View>
+        <View style={styles.container}>
+
+            <MaterialIcons name='lightbulb' size={150}
+                color={bulbOn ? 'yellow' : 'black'}
+            />
+            <Switch
+                value={bulbOn}
+                onChange={toggleSwitch}
+            ></Switch>
+        </View>
     );
 };
 
+const styles = StyleSheet.create({
+    container: { padding: 16, gap: 16, alignItems: 'center' },
+    buttonStyle: {
+        backgroundColor: '#abcdef',
+        padding: 8,
+        alignItems: 'center',
+        borderRadius: 8,
+    },
+
+    txtResult: { fontSize: 24, fontWeight: 'bold' },
+});
 export default BulbScreen;
