b93bfff479af02955acc0125785455f767fff24c
Muhammad Abid <abidroid1@gmail.com>
2025-05-17 15:34:36 +0500

    bulb status persisted

--- screens/BulbScreen.js
+++ screens/BulbScreen.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet, Switch } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const BulbScreen = () => {
@@ -8,6 +9,44 @@ const BulbScreen = () => {
     const [bulbOn, setBulbChange] = React.useState(false)
 
     const toggleSwitch = () => setBulbChange(previousState => !previousState);
+
+    const BULB_KEY = 'bulb_key';
+
+
+    useEffect(() => {
+
+        // load the status on component mount
+        const loadBulb = async () => {
+
+            try {
+                const value = await AsyncStorage.getItem(BULB_KEY);
+                if (value != null) {
+                    setBulbChange(value === 'true');
+                }
+            } catch (e) {
+
+            }
+        };
+
+        loadBulb();
+    }, []);
+
+
+    // save bulb status every time it changes
+    useEffect(() => {
+        const saveBulbStatus = async () => {
+            try{
+                AsyncStorage.setItem(BULB_KEY, bulbOn.toString());
+            }catch(e){
+
+            }
+
+        };
+        saveBulbStatus();
+
+    }, [bulbOn]);
+
+
 
     return (
         <View style={styles.container}>
