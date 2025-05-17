d19f997cfdf0ac650b27ca649c8da0232f5cd118
Muhammad Abid <abidroid1@gmail.com>
2025-05-17 14:47:57 +0500

    Counter Screen design

--- screens/CounterScreen.js
+++ screens/CounterScreen.js
@@ -1,9 +1,35 @@
-import {View, Text} from 'react-native';
+import React from 'react';
+import { View, Text, Button, StyleSheet, Pressable } from 'react-native';
 
 const CounterScreen = () => {
- return (
-        <View></View>
+
+    const [counter, setCounter] = React.useState(0)
+    return (
+        <View style={styles.container}>
+            <Pressable
+                onPress={() => setCounter(counter - 1)}
+
+                style={styles.buttonStyle}
+            ><Text>Decrement</Text></Pressable>
+            <Text style={styles.txtResult}>{counter}</Text>
+
+            <Pressable
+                onPress={() => setCounter(counter + 1)}
+                style={styles.buttonStyle}
+            ><Text>Increment</Text></Pressable>
+        </View>
     );
 };
 
+const styles = StyleSheet.create({
+    container: { padding: 16, gap: 16, alignItems: 'center' },
+    buttonStyle: {
+        backgroundColor: '#abcdef',
+        padding: 8,
+        alignItems: 'center',
+        borderRadius: 8,
+    },
+
+    txtResult: {fontSize: 24, fontWeight: 'bold'},
+});
 export default CounterScreen;
